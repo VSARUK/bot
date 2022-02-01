c9879134f3fe4e9079b82caa08f5da4f8b7daa6f
Sam Stealey <samstealey05@yahoo.com>
2022-02-01 21:26:19 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import discord
 import os
 from dotenv import load_dotenv
-
+from discord.ext import commands
 load_dotenv('secureCunt.env')
 TOKEN = os.getenv("DISCORD_TOKEN")
-##TOKEN = "OTM4MTcwNTU0NDMzODI2ODQ2.YfmZag.RNoQVZU2HN4p7g2rHQ7an6o5IZ0"
+#
 client = discord.Client()
 
 @client.event
 async def on_ready():
   print(f'{client.user} has connected to Discord!')
+@client.event
+async def hello( ):
+          await ctx.send("Hi")
 
 client.run(TOKEN)
